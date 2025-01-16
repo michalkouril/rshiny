79e0ffc0ee233d5a759fd336575f705b73331631
Michal Kouril <Michal.Kouril@cchmc.org>
2025-01-15 20:14:11 -0500

    Update app.R

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data - update"),
+    titlePanel("Old Faithful Geyser Data - update1"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
